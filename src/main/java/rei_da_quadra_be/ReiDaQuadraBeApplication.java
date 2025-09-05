0ff2c1ef4255836302ebac09b2683fcf6b88e829
JP <jplmarotinho22@gmail.com>
2025-09-05 19:46:50 -0300

    Repositório criado

---
+++ src/main/java/rei_da_quadra_be/ReiDaQuadraBeApplication.java
@@ -0,0 +1,13 @@
+package rei_da_quadra_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReiDaQuadraBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReiDaQuadraBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rei_da_quadra_be/ReiDaQuadraBeApplicationTests.java
@@ -0,0 +1,13 @@
+package rei_da_quadra_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReiDaQuadraBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
