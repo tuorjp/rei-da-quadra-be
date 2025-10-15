169f6e6ac4e768d67ec84abc1b6bd0f2074ad095
JP <jplmarotinho22@gmail.com>
2025-10-15 09:33:53 -0300

    EventoRepository

---
+++ src/main/java/rei_da_quadra_be/repository/EventoRepository.java
@@ -0,0 +1,11 @@
+package rei_da_quadra_be.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rei_da_quadra_be.model.Evento;
+import rei_da_quadra_be.model.User;
+
+import java.util.List;
+
+public interface EventoRepository extends JpaRepository<Long, Evento> {
+  List<Evento> findByUsuario(User usuario);
+}
