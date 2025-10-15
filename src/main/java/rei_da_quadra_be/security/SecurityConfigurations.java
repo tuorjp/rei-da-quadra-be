60adcf12f50f0112d47e33d332668ea54f2f086c
JP <jplmarotinho22@gmail.com>
2025-10-15 09:31:16 -0300

    SecurityConfigurations: liberação de endpoints do swagger

--- src/main/java/rei_da_quadra_be/security/SecurityConfigurations.java
+++ src/main/java/rei_da_quadra_be/security/SecurityConfigurations.java
@@ -29,6 +29,7 @@ public class SecurityConfigurations {
             )
             .authorizeHttpRequests(
                     authorize -> authorize
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                             .requestMatchers(HttpMethod.POST, "/test").hasRole("ADMIN")
